5263eadaa2bd3948be1719afe7423e2e8aa60d95
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2026-08-16 12:06:13 +1000

    refactor: use repeat block in concatenation of array solution

--- src/main/kotlin/problems/p001concatenationofarray/Solution.kt
+++ src/main/kotlin/problems/p001concatenationofarray/Solution.kt
@@ -2,11 +2,11 @@ package problems.p001concatenationofarray
 
 class Solution {
     fun getConcatenation(nums: IntArray): IntArray {
-        val ans = IntArray(nums.size * 2)
-        for (i in nums.indices) {
-            ans[i] = nums[i]
-            ans[i + nums.size] = nums[i]
+        val ans = mutableListOf<Int>()
+
+        repeat(2) {
+            ans.addAll(nums.toList())
         }
-        return ans
+        return ans.toIntArray()
     }
 }
